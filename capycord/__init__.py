c7b89a5422c08c9bd7a8912c368fc9dbc3a9b0b7
WardPearce <wardpearce@protonmail.com>
2022-04-18 19:22:06 +1200

    More progress on discord bot

--- capycord/__init__.py
+++ capycord/__init__.py
@@ -1,13 +1,23 @@
 import disnake
+import logging
+import asyncio
 
-from disnake.ext import commands
+from disnake.ext import commands, tasks
 from aiohttp import ClientSession
 from motor.motor_asyncio import AsyncIOMotorClient
+from sys import stdout
+from json import JSONDecodeError
 
 from .env import (
     CHECK_DELAY, MONGO_HOST, MONGO_PORT, MONGO_DB,
     CAPY_API_LINK, CAPY_LIFE_LINK
 )
+
+
+logger = logging.getLogger("capycord")
+logger.setLevel(logging.DEBUG)
+consoleHandler = logging.StreamHandler(stdout)
+logger.addHandler(consoleHandler)
 
 
 class CapyCog(commands.Cog):
@@ -21,13 +31,86 @@ class CapyCog(commands.Cog):
         )
         self.__collection = self.__mongo[MONGO_DB]
 
+        self.check_capy.start()
+
         return await super().cog_load()
 
     def cog_unload(self) -> None:
+        self.check_capy.cancel()
+
         self.bot.loop.create_task(
             self.__http.close()
         )
+
         return super().cog_unload()
+
+    async def __send_capy(self, guild_id: int, channel_id: int,
+                          name: str, image: str, capy_id: str) -> None:
+        guild = self.bot.get_guild(guild_id)
+        if not guild:
+            await self.__collection.channel.delete_many({
+                "guild_id": guild_id
+            })
+            return
+
+        channel = guild.get_channel(channel_id)
+        if not channel:
+            await self.__collection.channel.delete_many({
+                "guild_id": guild_id
+            })
+            return
+
+        if not isinstance(channel, disnake.TextChannel):
+            return
+
+        embed = disnake.Embed(
+            title=f"Meet {name}!",
+            url=CAPY_LIFE_LINK
+        )
+        embed.set_image(image)
+
+        try:
+            await channel.send(embed=embed)
+        except Exception:
+            pass
+
+        await self.__collection.channel.update_one({
+            "guild_id": guild_id
+        }, {"$set": {"last_capy_id": capy_id}})
+
+    @tasks.loop(seconds=CHECK_DELAY)
+    async def check_capy(self) -> None:
+        async with self.__http.get(CAPY_API_LINK) as resp:
+            if resp.status != 200:
+                logger.warn((
+                    f"HTTP Request to \"{CAPY_API_LINK}\""
+                    f" gave status code {resp.status}"
+                ))
+                return
+
+            try:
+                json = await resp.json()
+            except JSONDecodeError as error:
+                logger.warn(error)
+                return
+
+            query = {"last_capy_id": {
+                "$ne": json["_id"]
+            }}
+            async for record in self.__collection.channel.find(query):
+                asyncio.create_task(
+                    self.__send_capy(
+                        record["guild_id"],
+                        record["channel_id"],
+                        json["name"],
+                        json["image"],
+                        json["_id"]
+                    )
+                )
+
+    @check_capy.before_loop
+    async def before_printer(self):
+        await self.bot.wait_until_ready()
 
     @commands.slash_command(
         description="Interact with capy bot"
--- capycord/env.py
+++ capycord/env.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 CHECK_DELAY = int(os.getenv("CHECK_DELAY", 120))
 CAPY_LIFE_LINK = os.getenv("CAPY_LIFE_LINK", "https://capy.life")
-CAPY_API_LINK = os.getenv("CAPY_API_LINK", "https://capy.life/api/")
+CAPY_API_LINK = os.getenv("CAPY_API_LINK", "http://localhost/api/")
 
 MONGO_HOST = os.getenv("MONGO_IP", "localhost")
 MONGO_PORT = int(os.getenv("MONGO_PORT", 27017))
